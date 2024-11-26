c661fcc5f831c0a049a75b109e4f0da0ad72a449
Daniel <beteab.solomon@gmail.com>
2024-11-26 14:31:31 -0500

    Add prototype summary page

--- workBookManager.py
+++ workBookManager.py
@@ -1,6 +1,6 @@
 import xlsxwriter
 from typing import List, Tuple, Callable
-
+from time import time
 
 class WorkbookManager:
 
@@ -8,15 +8,20 @@ class WorkbookManager:
         self.workbookName = workbookName
         self.wb = xlsxwriter.Workbook(workbookName)
         
-        # TODO: add more worksheets here if/when necessary
-        self.mainSheet = self.wb.add_worksheet("MainSheet")
-        
+        self.summarySheet = self.wb.add_worksheet("Summary")
+        self.mainSheet = self.wb.add_worksheet("Main")
+
         # Freeze header row
         self.mainSheet.freeze_panes(1, 0)
-        
+
         # For if more sheets are used
         self.sheetRow = {}
         self.sheetRow[self.mainSheet] = 1
+
+        # Summary sheet metrics
+        self.filesScannedCount = 0
+        self.executionTime = 0
+        self.errorCount = 0
 
         # An empty function by default. Called within fileCrawl()
         self.checkMethod = lambda: None
@@ -44,6 +49,9 @@ class WorkbookManager:
         self.mainSheet.set_column(self.ITEM_COL, self.RENAME_COL, 30)
         self.mainSheet.set_column(self.ERROR_COL, self.ERROR_COL+5, 20)  # +5 for adding more than one error
 
+        self.summarySheet.set_column(0, 1, 20)
+
+
         # Default cell styles
         self.dirColFormat = self.wb.add_format({
             "bg_color": "#99CCFF", # blueish
@@ -70,21 +78,35 @@ class WorkbookManager:
             "bold": True
         })
 
+
+        self.summaryValueFormat = self.wb.add_format({
+            "bold": True
+        })
+
+
         # Write headers
         self.mainSheet.write(0, self.DIR_COL, "Directories", self.headerFormat)
         self.mainSheet.write(0, self.ITEM_COL, "Items", self.headerFormat)
         self.mainSheet.write(0, self.RENAME_COL, "Rename", self.headerFormat)
         self.mainSheet.write(0, self.ERROR_COL, "Errors", self.headerFormat)
 
+        self.summarySheet.write(0, 0, "File count", self.headerFormat)
+        self.summarySheet.write(1, 0, "Error count", self.headerFormat)
+        self.summarySheet.write(2, 0, "Error percentage (%)", self.headerFormat)
+        self.summarySheet.write(3, 0, "Execution time (s)", self.headerFormat)
+
 
 
     def fileCrawl(self, dirAbsolute, dirItems: List[str]):
         # can be used to write down folders as well, hence "items" and not "files"
         for itemName in dirItems:
-            
+
             # If error present
             if (self.checkMethod(dirAbsolute, itemName)):
                 self.mainSheet.write(self.sheetRow[self.mainSheet], self.ITEM_COL, itemName, self.fileErrorFormat)
+                
+                self.errorCount += 1
+
             else:
                 self.mainSheet.write(self.sheetRow[self.mainSheet], self.ITEM_COL, itemName)
 
@@ -93,9 +115,16 @@ class WorkbookManager:
 
 
     def folderCrawl(self, dirTree: List[Tuple[str, list, list]]):
+        start = time()
+        
         for (dirAbsolute, dirFolders, dirFiles) in dirTree:
             self.mainSheet.write(self.sheetRow[self.mainSheet], self.DIR_COL, dirAbsolute, self.dirColFormat)
             self.fileCrawl(dirAbsolute, dirFiles)
+
+            self.filesScannedCount += len(dirFiles)
+        
+        end = time()
+        self.executionTime = end - start
 
 
 
@@ -110,5 +139,16 @@ class WorkbookManager:
 
 
 
+    def populateSummarySheet(self):
+        errorPercentage = round(self.errorCount / self.filesScannedCount * 100)
+
+        self.summarySheet.write_number(0, 1, self.filesScannedCount, self.summaryValueFormat)
+        self.summarySheet.write_number(1, 1, self.errorCount, self.summaryValueFormat)
+        self.summarySheet.write_number(2, 1, round(errorPercentage, 4), self.summaryValueFormat)
+        self.summarySheet.write_number(3, 1, round(self.executionTime, 4), self.summaryValueFormat)
+        
+
+
     def close(self):
+        self.populateSummarySheet()
         self.wb.close()
