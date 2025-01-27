f193d1b941b812595144a4e2703fdc59c5793725
Daniel <beteab.solomon@gmail.com>
2025-01-27 14:34:02 -0500

    Add dynamic admin functionality

--- fileCrawler.py
+++ fileCrawler.py
@@ -65,7 +65,7 @@ def control(dirAbsolute:str, includeSubfolders:bool, modify:bool, selectedFindPr
 
 
 
-def view():
+def view(isAdmin: bool):
     def launchController():
         if modifyState.get() and not tk.messagebox.askyesnocancel("Allow Modify?", "You have chosen to modify items. This is an IRREVERSIBLE action. Are you sure?"):
             # print("Aborted. Continuing selection.")
@@ -74,7 +74,7 @@ def view():
         root.title(FILE_CRAWLER + ": CURRENTLY RUNNING...")
         exitCode = control(dirAbsoluteVar.get(), bool(includeSubFoldersState.get()), bool(modifyState.get()), 
                            [findListbox.get(fm) for fm in findListbox.curselection()],
-                           fixListbox.get(tk.ACTIVE) if isAdmin else NULL_OPTION,
+                           fixListbox.get(tk.ACTIVE),
                            parameterVar.get())
         root.title(FILE_CRAWLER)
         
@@ -107,26 +107,45 @@ def view():
             tk.messagebox.showinfo("Directory DNE", "Directory \"" + RESULTS_DIRECTORY + "\" does not exist. Try executing a file crawl first.")
 
 
-    isAdmin = False
-    if (len(argv) <= 1):
-        pass
-    elif (argv[1] != PASSWORD):
-        tk.messagebox.showerror("FileCrawler: INCORRECT", "Incorrect.")
-        return
-    else:
-        isAdmin = True
+    def checkPassword():
+        if passwordEntryVar.get() == PASSWORD:
+            addAdminLayout(includeSubfoldersCheckbutton.winfo_height())
+            root.unbind("<Control-Key-p>")
+        else:
+            tk.messagebox.showerror("FileCrawler: INCORRECT", "Incorrect.")
+        
+
+    def inquirePassword(event):
+        passwordWindow = tk.Toplevel(root)
+        passwordWindow.title("Type password")
+        passwordWindow.focus_set()
+
+        # passwordWindow.lift(root)  # -> Put over root
+
+        passwordEntry = tk.Entry(passwordWindow, textvariable=passwordEntryVar, width=30, font=fontGeneral)
+        passwordEntry.pack()
+        passwordEntry.focus_set()
+
+        passwordEntry.bind("<Return>", lambda e: [
+            checkPassword(), passwordEntryVar.set(""), passwordWindow.destroy()])
     
+
+    def addAdminLayout(extraHeight):
+        root.geometry("{}x{}".format(rootWidth, rootHeight + extraHeight))
+        root.attributes('-topmost', True)  # keeps root window at top layer
+
+        modifyCheckbutton = tk.Checkbutton(frames[5], text="Allow Modify", variable=modifyState, font=fontGeneral)
+        modifyCheckbutton.pack(padx=(0, 50))
+        
+
     # root stuff
     root = tk.Tk()
     root.title(FILE_CRAWLER)
     root.resizable(0, 0)
-    rootWidth = 500 if isAdmin else 300
-    rootHeight = 430 if isAdmin else 350
+    rootWidth = 500
+    rootHeight = 390
     root.geometry("{}x{}".format(rootWidth, rootHeight))
-
-    if isAdmin:
-        root.attributes('-topmost', True)  # keeps root window at top layer
-
+        
     frames = []
     for i in range(8):
         frames.append(tk.Frame(root, bd=0, relief=tk.SOLID))
@@ -138,9 +157,9 @@ def view():
     fontSize = 15
     fontGeneral = (fontType, fontSize)
     fontSmall = (fontType, int(fontSize/3*2))
-    listboxHeight = max(len(FIND_PROCEDURES.keys()), len(FIX_PROCEDURES.keys()))+1 if isAdmin else len(FIND_PROCEDURES.keys())+1
-    listboxWidth = int(rootWidth/16) if isAdmin else int(rootWidth/10)
-    finalButtonsWidth = 20 if isAdmin else 10
+    listboxHeight = max(len(FIND_PROCEDURES.keys()), len(FIX_PROCEDURES.keys())) +1
+    listboxWidth = int(rootWidth/15)
+    finalButtonsWidth = 20
 
     # data variables
     dirAbsoluteVar = tk.StringVar()
@@ -159,42 +178,33 @@ def view():
     dirLabel.pack(side=tk.LEFT)
     
     findLabel = tk.Label(frames[2], text="Find", font=fontGeneral)
-    if isAdmin:
-        fixLabel = tk.Label(frames[2], text="Fix", font=fontGeneral)
-        fixLabel.pack(side=tk.RIGHT, padx=(0, rootWidth/6))
-        findLabel.pack(side=tk.LEFT, padx=(rootWidth/6, 0))
-    else:
-        findLabel.pack()
+    fixLabel = tk.Label(frames[2], text="Fix", font=fontGeneral)
+    fixLabel.pack(side=tk.RIGHT, padx=(0, rootWidth/5))
+    findLabel.pack(side=tk.LEFT, padx=(rootWidth/5, 0))
+
     frames[2].pack(fill="x", padx=10, pady=(3, 0))  # inadvertently packed twice to have less y padding
 
     findListbox = tk.Listbox(frames[3], selectmode="multiple", exportselection=0, width=listboxWidth, height=listboxHeight)
+    fixListbox = tk.Listbox(frames[3], exportselection=0, width=listboxWidth, height=listboxHeight)
     for findProcedureName in FIND_PROCEDURES.keys():
         findListbox.insert(tk.END, findProcedureName)
+    for fixProcedureName in FIX_PROCEDURES.keys():
+        fixListbox.insert(tk.END, fixProcedureName)
     findListbox.select_set(0)
+    fixListbox.select_set(0)
     findListbox.config(font=fontSmall)
-    if isAdmin:
-        fixListbox = tk.Listbox(frames[3], exportselection=0, width=listboxWidth, height=listboxHeight)
-        for fixProcedureName in FIX_PROCEDURES.keys():
-            fixListbox.insert(tk.END, fixProcedureName)
-        fixListbox.select_set(0)
-        fixListbox.config(font=fontSmall)
-        findListbox.pack(side=tk.LEFT)
-        fixListbox.pack(side=tk.RIGHT)
-    else:
-        findListbox.pack()
+    fixListbox.config(font=fontSmall)
+    findListbox.pack(side=tk.LEFT)
+    fixListbox.pack(side=tk.RIGHT)
     frames[3].pack(fill="x", padx=10, pady=(0, 3))
 
-    if isAdmin:
-        parameterLabel = tk.Label(frames[4], text="Parameter:", font=fontGeneral)
-        argumentEntry = tk.Entry(frames[4], textvariable=parameterVar, width=rootWidth, font=fontSmall)
-        parameterLabel.pack(side=tk.LEFT)
-        argumentEntry.pack(side=tk.LEFT)
+    parameterLabel = tk.Label(frames[4], text="Parameter:", font=fontGeneral)
+    argumentEntry = tk.Entry(frames[4], textvariable=parameterVar, width=rootWidth, font=fontSmall)
+    parameterLabel.pack(side=tk.LEFT)
+    argumentEntry.pack(side=tk.LEFT)
 
     includeSubfoldersCheckbutton = tk.Checkbutton(frames[5], text="Include Subfolders", variable=includeSubFoldersState, font=fontGeneral)
     includeSubfoldersCheckbutton.pack()
-    if isAdmin:
-        modifyCheckbutton = tk.Checkbutton(frames[5], text="Allow Modify", variable=modifyState, font=fontGeneral)
-        modifyCheckbutton.pack(padx=(0, 50))  # HARD CODED PADDING
 
     executeButton = tk.Button(frames[6], text="Execute", command=launchController, width=finalButtonsWidth, font=fontGeneral)
     executeButton.pack()
@@ -206,11 +216,24 @@ def view():
     frames[7].configure(width=rootWidth/2)
     frames[7].pack(side=tk.LEFT, expand=True)
 
+    if isAdmin:
+        # root.after(50, addAdminLayout)
+        addAdminLayout(35)
+    else:
+        passwordEntryVar = tk.StringVar()
+        root.bind('<Control-Key-p>', inquirePassword)
+    
     root.mainloop()
         
 
 def main():
-    view()
+    if (len(argv) <= 1):
+        view(False)
+    elif (argv[1] != PASSWORD):
+        tk.messagebox.showerror("FileCrawler: INCORRECT", "Incorrect.")
+        return
+    else:
+        view(True)
 
 
 if __name__ == "__main__":
