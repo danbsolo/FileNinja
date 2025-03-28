5dc2d0d00ec2dc551d343a5f80e49c2a65b12a26
Daniel Solomon <daniel.solomon@pc.gc.ca>
2025-03-28 11:00:09 -0400

    Optimize finding of owner

--- findOwner.py
+++ findOwner.py
@@ -1,211 +1,111 @@
-import ctypes as ctypes
-from ctypes import wintypes as wintypes
+import ctypes
+from ctypes import wintypes
 
 kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
 advapi32 = ctypes.WinDLL('advapi32', use_last_error=True)
 
-ERROR_INVALID_FUNCTION  = 0x0001
-ERROR_FILE_NOT_FOUND    = 0x0002
-ERROR_PATH_NOT_FOUND    = 0x0003
-ERROR_ACCESS_DENIED     = 0x0005
-ERROR_SHARING_VIOLATION = 0x0020
-
+# Constants
 SE_FILE_OBJECT = 1
 OWNER_SECURITY_INFORMATION = 0x00000001
-GROUP_SECURITY_INFORMATION = 0x00000002
-DACL_SECURITY_INFORMATION  = 0x00000004
-SACL_SECURITY_INFORMATION  = 0x00000008
-LABEL_SECURITY_INFORMATION = 0x00000010
 
-_DEFAULT_SECURITY_INFORMATION = (OWNER_SECURITY_INFORMATION |
-    GROUP_SECURITY_INFORMATION | DACL_SECURITY_INFORMATION |
-    LABEL_SECURITY_INFORMATION)
+# Mapping for SID type values
+sid_type_map = {
+    1: 'User',
+    2: 'Group',
+    3: 'Domain',
+    4: 'Alias',
+    5: 'WellKnownGroup',
+    6: 'DeletedAccount',
+    7: 'Invalid',
+    8: 'Unknown',
+    9: 'Computer',
+    10: 'Label'
+}
 
-LPDWORD = ctypes.POINTER(wintypes.DWORD)
-SE_OBJECT_TYPE = wintypes.DWORD
-SECURITY_INFORMATION = wintypes.DWORD
-
-class SID_NAME_USE(wintypes.DWORD):
-    _sid_types = dict(enumerate('''
-        User Group Domain Alias WellKnownGroup DeletedAccount
-        Invalid Unknown Computer Label'''.split(), 1))
-
-    def __init__(self, value=None):
-        if value is not None:
-            if value not in self.sid_types:
-                raise ValueError('invalid SID type')
-            wintypes.DWORD.__init__(value)
-
-    def __str__(self):
-        if self.value not in self._sid_types:
-            raise ValueError('invalid SID type')
-        return self._sid_types[self.value]
-
-    def __repr__(self):
-        return 'SID_NAME_USE(%s)' % self.value
-
-PSID_NAME_USE = ctypes.POINTER(SID_NAME_USE)
-
-class PLOCAL(wintypes.LPVOID):
-    _needs_free = False
-    def __init__(self, value=None, needs_free=False):
-        super(PLOCAL, self).__init__(value)
-        self._needs_free = needs_free
-
-    def __del__(self):
-        if self and self._needs_free:
-            kernel32.LocalFree(self)
-            self._needs_free = False
-
-PACL = PLOCAL
-
-class PSID(PLOCAL):
-    def __init__(self, value=None, needs_free=False):
-        super(PSID, self).__init__(value, needs_free)
-
-    def __str__(self):
-        if not self:
-            raise ValueError('NULL pointer access')
-        sid = wintypes.LPWSTR()
-        advapi32.ConvertSidToStringSidW(self, ctypes.byref(sid))
-        try:
-            return sid.value
-        finally:
-            if sid:
-                kernel32.LocalFree(sid)
-
-class PSECURITY_DESCRIPTOR(PLOCAL):
-    def __init__(self, value=None, needs_free=False):
-        super(PSECURITY_DESCRIPTOR, self).__init__(value, needs_free)
-        self.pOwner = PSID()
-        self.pGroup = PSID()
-        self.pDacl = PACL()
-        self.pSacl = PACL()
-        # back references to keep this object alive
-        self.pOwner._SD = self
-        self.pGroup._SD = self
-        self.pDacl._SD = self
-        self.pSacl._SD = self
-
-    def get_owner(self, system_name=None):
-        if not self or not self.pOwner:
-            raise ValueError('NULL pointer access')
-        return look_up_account_sid(self.pOwner, system_name)
-
-    def get_group(self, system_name=None):
-        if not self or not self.pGroup:
-            raise ValueError('NULL pointer access')
-        return look_up_account_sid(self.pGroup, system_name)
-
-def _check_bool(result, func, args):
-    if not result:
-        raise ctypes.WinError(ctypes.get_last_error())
-    return args
-
-# msdn.microsoft.com/en-us/library/aa376399
-advapi32.ConvertSidToStringSidW.errcheck = _check_bool
-advapi32.ConvertSidToStringSidW.argtypes = (
-    PSID, # _In_   Sid
-    ctypes.POINTER(wintypes.LPWSTR)) # _Out_ StringSid
-
-# msdn.microsoft.com/en-us/library/aa379166
-advapi32.LookupAccountSidW.errcheck = _check_bool
-advapi32.LookupAccountSidW.argtypes = (
-    wintypes.LPCWSTR, # _In_opt_  lpSystemName
-    PSID,             # _In_      lpSid
-    wintypes.LPCWSTR, # _Out_opt_ lpName
-    LPDWORD,          # _Inout_   cchName
-    wintypes.LPCWSTR, # _Out_opt_ lpReferencedDomainName
-    LPDWORD,          # _Inout_   cchReferencedDomainName
-    PSID_NAME_USE)    # _Out_     peUse
-
-# msdn.microsoft.com/en-us/library/aa446645
+# Set up GetNamedSecurityInfoW function prototype
 advapi32.GetNamedSecurityInfoW.restype = wintypes.DWORD
-advapi32.GetNamedSecurityInfoW.argtypes = (
-    wintypes.LPWSTR,       # _In_      pObjectName
-    SE_OBJECT_TYPE,        # _In_      ObjectType
-    SECURITY_INFORMATION,  # _In_      SecurityInfo
-    ctypes.POINTER(PSID),  # _Out_opt_ ppsidOwner
-    ctypes.POINTER(PSID),  # _Out_opt_ ppsidGroup
-    ctypes.POINTER(PACL),  # _Out_opt_ ppDacl
-    ctypes.POINTER(PACL),  # _Out_opt_ ppSacl
-    ctypes.POINTER(PSECURITY_DESCRIPTOR)) # _Out_opt_ ppSecurityDescriptor
+advapi32.GetNamedSecurityInfoW.argtypes = [
+    wintypes.LPWSTR,         # pObjectName
+    wintypes.DWORD,          # ObjectType
+    wintypes.DWORD,          # SecurityInfo (only OWNER requested)
+    ctypes.POINTER(ctypes.c_void_p),  # ppsidOwner
+    ctypes.POINTER(ctypes.c_void_p),  # ppsidGroup (unused)
+    ctypes.POINTER(ctypes.c_void_p),  # ppDacl (unused)
+    ctypes.POINTER(ctypes.c_void_p),  # ppSacl (unused)
+    ctypes.POINTER(ctypes.c_void_p)   # ppSecurityDescriptor
+]
 
+# Set up LookupAccountSidW function prototype
+advapi32.LookupAccountSidW.restype = wintypes.BOOL
+advapi32.LookupAccountSidW.argtypes = [
+    wintypes.LPCWSTR,         # lpSystemName (None for local)
+    ctypes.c_void_p,          # Sid
+    wintypes.LPWSTR,          # Name buffer
+    ctypes.POINTER(wintypes.DWORD), # cchName
+    wintypes.LPWSTR,          # Domain buffer
+    ctypes.POINTER(wintypes.DWORD), # cchDomainName
+    ctypes.POINTER(wintypes.DWORD)  # peUse (SID type)
+]
 
-
-def look_up_account_sid(sid, system_name=None):
-    SIZE = 256
-    name = ctypes.create_unicode_buffer(SIZE)
-    domain = ctypes.create_unicode_buffer(SIZE)
-    cch_name = wintypes.DWORD(SIZE)
-    cch_domain = wintypes.DWORD(SIZE)
-    sid_type = SID_NAME_USE()
-    advapi32.LookupAccountSidW(system_name, sid, name, ctypes.byref(cch_name),
-        domain, ctypes.byref(cch_domain), ctypes.byref(sid_type))
-    return name.value, domain.value, sid_type
-
-def get_file_security(filename, request=_DEFAULT_SECURITY_INFORMATION):
-    # N.B. This query may fail with ERROR_INVALID_FUNCTION
-    # for some filesystems.
-    pSD = PSECURITY_DESCRIPTOR(needs_free=True)
-    error = advapi32.GetNamedSecurityInfoW(filename, SE_FILE_OBJECT, request,
-                ctypes.byref(pSD.pOwner), ctypes.byref(pSD.pGroup),
-                ctypes.byref(pSD.pDacl), ctypes.byref(pSD.pSacl),
-                ctypes.byref(pSD))
+def get_file_owner_info(filename):
+    # Ensure the filename is a Unicode string.
+    if not isinstance(filename, str):
+        filename = filename.decode('utf-8')
+    
+    # Prepare pointers for owner SID and security descriptor.
+    pOwner = ctypes.c_void_p()
+    pSD = ctypes.c_void_p()
+    
+    error = advapi32.GetNamedSecurityInfoW(
+        filename,
+        SE_FILE_OBJECT,
+        OWNER_SECURITY_INFORMATION,
+        ctypes.byref(pOwner),
+        None, None, None,
+        ctypes.byref(pSD)
+    )
     if error != 0:
         raise ctypes.WinError(error)
-    return pSD
-
+    
+    # Prepare buffers for account name and domain.
+    name_buf = ctypes.create_unicode_buffer(256)
+    domain_buf = ctypes.create_unicode_buffer(256)
+    name_size = wintypes.DWORD(256)
+    domain_size = wintypes.DWORD(256)
+    sid_type = wintypes.DWORD()
+    
+    if not advapi32.LookupAccountSidW(
+        None, pOwner, name_buf, ctypes.byref(name_size),
+        domain_buf, ctypes.byref(domain_size), ctypes.byref(sid_type)
+    ):
+        raise ctypes.WinError(ctypes.get_last_error())
+    
+    # Free the security descriptor memory.
+    kernel32.LocalFree(pSD)
+    
+    sid_type_str = sid_type_map.get(sid_type.value, f"Unknown({sid_type.value})")
+    return f"{domain_buf.value}\\{name_buf.value} ({sid_type_str})"
 
 def getOwner(dirAbsolute):
-    if isinstance(dirAbsolute, bytes):
-        if hasattr(os, 'fsdecode'):
-            dirAbsolute = os.fsdecode(dirAbsolute)
-        else:
-            dirAbsolute = dirAbsolute.decode(sys.getfilesystemencoding())
-
-    pSD = get_file_security(dirAbsolute)
-    owner_name, owner_domain, owner_sid_type = pSD.get_owner()
-    if owner_domain:
-        return '{}\\{} ({})'.format(owner_domain, owner_name, owner_sid_type)
-
-    #print("Path : {}".format(filename))
-    #print("Owner: {} ({})".format(owner_name, owner_sid_type))
-    #print("SID  : {}".format(pSD.pOwner))
+    """Return the owner info in 'DOMAIN\\Owner (SID_Type)' format."""
+    return get_file_owner_info(dirAbsolute)
 
 def getOwnerCatch(dirAbsolute):
+    """Wrapper that adjusts for UNC paths and catches exceptions."""
     if dirAbsolute.startswith('\\\\'):
         dirAbsolute = '\\\\?\\UNC' + dirAbsolute[1:]
     else:
         dirAbsolute = '\\\\?\\' + dirAbsolute
 
     try:
-        ownerOutput = getOwner(dirAbsolute)
+        return getOwner(dirAbsolute)
     except Exception as e:
-        ownerOutput = "GET OWNER FAILED: {}".format(e)
-    
-    return ownerOutput
+        return f"GET OWNER FAILED: {e}"
 
-
+# Example usage:
 if __name__ == '__main__':
-    import os, sys
-
+    import sys
     if len(sys.argv) < 2:
-        script_name = os.path.basename(__file__)
-        sys.exit('usage: {} filename'.format(script_name))
-
+        sys.exit(f"Usage: {sys.argv[0]} <filename>")
     filename = sys.argv[1]
-    if isinstance(filename, bytes):
-        if hasattr(os, 'fsdecode'):
-            filename = os.fsdecode(filename)
-        else:
-            filename = filename.decode(sys.getfilesystemencoding())
-
-    pSD = get_file_security(filename)
-    owner_name, owner_domain, owner_sid_type = pSD.get_owner()
-    if owner_domain:
-        owner_name = '{}\\{}'.format(owner_domain, owner_name)
-
-    print("Path : {}".format(filename))
-    print("Owner: {} ({})".format(owner_name, owner_sid_type))
-    print("SID  : {}".format(pSD.pOwner))
+    print("Result:", getOwnerCatch(filename))
